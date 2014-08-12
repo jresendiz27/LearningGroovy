f20b1dccd320a5510593406026c96467fe428104
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2014-08-12 18:02:55 +0000

    Avances

--- files/lecturaTXTBanamex.groovy
+++ files/lecturaTXTBanamex.groovy
@@ -23,20 +23,32 @@ try{
                 throw new Exception("El archivo de concentracion de Banamex es erroneo, error en la linea ${contadorLineas}") 
                 return
             } else {
-                println lineaDeArchivo
                 iDia = Integer.parseInt(lineaDeArchivo.substring(0,2))
                 iMes = Integer.parseInt(lineaDeArchivo.substring(2,4))
                 iAnio = 2000 + Integer.parseInt(lineaDeArchivo.substring(4))
                 calendar.set(iAnio,iMes-1,iDia)
                 iFecha = calendar.getTime()
-                println "---> ${iDia} - ${iMes} - ${iAnio} == ${iFecha}"
+                //println "---> ${iDia} - ${iMes} - ${iAnio} == ${iFecha}"
             }
         } else if( contadorLineas == 2 ){
             if(lineaDeArchivo.length() != 29 ){
                 throw new Exception("El archivo de concentracion de Banamex es erroneo, error en la linea ${contadorLineas}") 
                 return
             } else {
-                
+                println lineaDeArchivo
+                iSuc = lineaDeArchivo.substring(0,4)
+                iCta = lineaDeArchivo.substring(4,23)
+                iMov = lineaDeArchivo.substring(23)
+                iCta = iSuc + "-" + (String.format("%07d", Integer.parseInt(iCta)))
+                //println "---> ${iSuc} - ${iCta} - ${iMov}"
+                def chequera
+                /*
+                def chequera = Chequera.findByCuentaCasa(iCta)            
+                */
+                if(!chequera){
+                    throw new Exception("No se encuentra la chequera con cuenta ${iCta}") 
+                    return
+                }
             }
         }
         contadorLineas++ 
