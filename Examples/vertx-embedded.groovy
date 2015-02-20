252cc8827e02d0b00d023ab651e601be33ffc32b
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-02-19 19:51:54 -0600

    Fix Update #1, Main class added
    Creating class and main method for preventing server hang out.

--- Examples/vertx-embedded.groovy
+++ Examples/vertx-embedded.groovy
@@ -2,15 +2,20 @@
 @Grab(group='io.vertx', module='vertx-platform', version='2.1.4')
 @Grab(group='io.vertx', module='lang-groovy', version='2.1.1-final')
 
-import org.vertx.groovy.core.http.RouteMatcher
 import org.vertx.groovy.core.Vertx;
 
-def server = Vertx.newVertx().createHttpServer()
-def port = System.getenv('PORT') != null ? System.getenv('PORT') as int : 8080
+class TestingVertx{
+  static void main(String[] args){
+    def server = Vertx.newVertx().createHttpServer()
+    def port = System.getenv('PORT') != null ? System.getenv('PORT') as int : 8080
 
-server.requestHandler { request ->
-    request.response.putHeader("Content-Type", "text/plain")
-    request.response.end("hello, world")
+    server.requestHandler { request ->
+        request.response.putHeader("Content-Type", "text/plain")
+        request.response.end("hello, world")
+    }
+    
+    server.listen(port, '0.0.0.0')
+    println "Server created at 8080"
+    System.in.read();
+  }
 }
-
-server.listen(port, '0.0.0.0')
