297fbd12d9e645e3a9e56d2f5ee061a0b60411b3
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-02-19 19:37:28 -0600

    Working on #1, Creating HttpBuilder Example
    Working with twitter api, creating a basic tweet using OAuth and HttpBuilder

---
+++ Examples/Twitter-HttpBuilder.groovy
@@ -0,0 +1,21 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
+@Grab(group='oauth.signpost', module='signpost-core', version='1.2.1.2')
+@Grab(group='oauth.signpost', module='signpost-commonshttp4', version='1.2.1.2')
+
+/*
+API Key : bCLQiug4hXKNkmgmsGzHuQ
+API Secret : DWLBdpiqmO9GR60LjPKQ3A0vs2KpiNFXk8JJsjVXoIQ
+*/
+import groovyx.net.http.RESTClient
+import static groovyx.net.http.ContentType.*
+
+def consumerKey = ""
+def consumerSecret = ""
+def accessToken = ""
+def secretToken = ""
+def twitter = new RESTClient( 'https://api.twitter.com/1.1/statuses/' )
+twitter.auth.oauth(consumerKey, consumerSecret, accessToken, secretToken)
+def postBody=[status: "Tweet from HttpBuilder and Twitter API!!! ${new Date()}"]
+def response = twitter.post( path : 'update.json', body: postBody, requestContentType: URLENC )
+println response.body.id
+println response.body.url
