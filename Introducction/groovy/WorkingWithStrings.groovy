920ef47e9ede26cedd64bb3dce3eb641b2911869
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-04-08 00:41:39 -0500

    Simple example of working with strings

---
+++ Introducction/groovy/WorkingWithStrings.groovy
@@ -0,0 +1,22 @@
+/*
+@author: Juan Alberto Reséndiz Arteaga
+Simple example of string management with Groovy!
+*/
+def cadena="Una cadena de caracteres"
+// Inferencia automática del tipo de variable!!!
+println cadena.class.name
+
+def unaCadenaMasCompleja = """
+Una cadena multilinea
+yay!
+ """
+//Automaticamente infiere también el primitivo de Java 
+println unaCadenaMasCompleja.class.name
+
+/*
+Ahora intentemos con una cadena compuesta, se les conoce como GStrings
+*/
+def nombre = "Pepo"
+def otraCadena = "Mi nombre es ${nombre}"
+println otraCadena.class.name
+println otraCadena
