6ab09aa9557475b83ccff2879001e3315a35ed7b
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-04-24 09:51:37 -0500

    Working with threads

--- Examples/groovy/DatabaseExecutorPool.groovy
+++ Examples/groovy/DatabaseExecutorPool.groovy
@@ -3,37 +3,27 @@
 @Grab(group='mysql', module='mysql-connector-java', version='5.1.35')
 
 import groovy.sql.Sql
-//import groovyx.gpars.GParsPool;
-//import groovyx.gpars.GParsExecutorsPool;
 import groovyx.gpars.*;
 
-sqlInstance = Sql.newInstance('jdbc:mysql://localhost:3306/randomInsert','root','n0m3l0s3','com.mysql.jdbc.Driver')
-
-def randomLimits = [250,17,100,117]
+def randomLimits = [500,700,800,750]
 def insert(def randomValue){
+    def sqlInstance = Sql.newInstance('jdbc:mysql://localhost:3306/randomInsert','root','n0m3l0s3','com.mysql.jdbc.Driver')
     def currentThread = ""+Thread.currentThread()+""
+    def random = (int) randomValue*Math.random()
+    def sum = (1..random).sum()
     for(i in 1..50000){
-         sqlInstance.execute("""insert into container(randomNumber,originThread) values (${(randomValue)*Math.random()}, $currentThread)""")   
+        sqlInstance.execute("""insert into container(randomNumber,originThread, sum) values ($random, $currentThread, $sum)""")   
     }
-    println currentThread
 }
-/*
-GParsPool.withPool{
-    randomLimits.eachParallel{
-        insert(it)
-    }
-}*/
-GParsExecutorsPool.withPool(4){
-	/*GParsExecutorsPool.executeAsyncAndWait(
-		{insert(250)},
-		{insert(17)},
-		{insert(100)},
-		{insert(117)}
-		)*/
+// Workers/Executor Pool
+def inicio = System.currentTimeMillis()
+GParsExecutorsPool.withPool{
     randomLimits.eachParallel{
         insert(it)
     }
 }
+def fin = System.currentTimeMillis()
+println ("[GParsExecutorsPool] Tiempo de ejecución: ${fin-inicio} ms")
 
 /*
 println "***************************************************************************************"
