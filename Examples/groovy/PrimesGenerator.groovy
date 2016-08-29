0690a96d6c167314e903a9f3af296143c7ec7c72
Juan Alberto Resendiz Arteaga <jresendiz@nearsoft.com>
2016-08-28 23:49:57 -0500

    Adding PrimesGenerator function to groovy snippets, moving example class with JUnit outside package

---
+++ Examples/groovy/PrimesGenerator.groovy
@@ -0,0 +1,19 @@
+def generatePrimesUpTo(int upperLimit) {
+    def primes = []
+    def numbers = 2..upperLimit
+    for(number in numbers) {
+        boolean isComposite = false
+        boolean isEven = false
+        for(prime in primes) {
+            if(number % prime == 0) {
+                isComposite = true
+                break;
+            }
+        }
+        if(!isComposite) {
+            primes.add(number)
+            continue;
+        }
+    }
+    return primes
+}
---
+++ Testing/src/main/groovy/com/nearsoft/apprentice/tdd/examples/FizzBuzz.groovy
@@ -0,0 +1,7 @@
+package com.nearsoft.apprentice.tdd.example1
+
+/**
+ * Created by jresendiz on 28/08/16.
+ */
+class FizzBuzz {
+}
